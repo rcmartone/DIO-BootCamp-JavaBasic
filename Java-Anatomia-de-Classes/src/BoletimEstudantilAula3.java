c4013234ed5e7bd3b7c5cb8af6fe32e10469dbc4
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-08-28 17:01:29 -0300

    Aula4_Anatomia_de_Classes_Part2

--- Java-Anatomia-de-Classes/src/BoletimEstudantilAula3.java
+++ Java-Anatomia-de-Classes/src/BoletimEstudantilAula3.java
@@ -1,11 +1,11 @@
 public class BoletimEstudantilAula3 {
-public static void main(String[] args) {
-int mediaFinal = 6;
-if(mediaFinal<6)	
-System.out.println("REPROVADO"); 
-else if(mediaFinal==6)
-System.out.println("PROVA MINERVA"); 
-else
-System.out.println("APROVADO"); 		
-}
+    public static void main(String[] args) {
+		int mediaFinal = 6;
+		if (mediaFinal < 6)
+			System.out.println("REPROVADO");
+		else if (mediaFinal == 6)
+			System.out.println("PROVA MINERVA");
+		else
+			System.out.println("APROVADO");
+	}
 }
