124ddf67ac3f26a51df949c6d5cd7b4120e7444e
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-08-28 16:43:57 -0300

    Aula2_Anatomia_de_Classes_Java

---
+++ Java-Anatomia-de-Classes/src/MinhaClasse.java
@@ -0,0 +1,11 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+       //Teste de nomeclatura de variaveis
+       //int long numero; Nomenclatura invalida, duas palavras reservadas
+       //String %arroz; Nomenclatura invalida, apenas _ e $ podem ser presentes
+       //int 1arroz; Nomenclatura invalida, não pode começar com numeros
+    }
+
+
+    
+}
