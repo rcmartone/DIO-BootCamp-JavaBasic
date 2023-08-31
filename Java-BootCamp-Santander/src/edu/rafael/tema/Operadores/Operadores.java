38663918751e172a3738e48d2e35644f2484cab4
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-08-31 16:46:27 -0300

    Aula3_Operadores_Java

--- Java-BootCamp-Santander/src/edu/rafael/tema/Operadores/Operadores.java
+++ Java-BootCamp-Santander/src/edu/rafael/tema/Operadores/Operadores.java
@@ -2,46 +2,28 @@ package edu.rafael.tema.Operadores;
 
 public class Operadores {
     public static void main(String[] args) {
-        //Atribuição
-        String nome = "GLEYSON";
-        int idade = 22;
-        double peso = 68.5;
-        char sexo = 'M';
-        boolean doadorOrgao = false;
 
-        //Aritméticos
-        double soma = 10.5 + 15.7;
-        int subtração = 113 - 25;
-        int multiplicacao = 20 * 7;
-        int divisao = 15 / 3;
-        int modulo = 18 % 3;
-        double resultado = (10 * 7) + (20/4);
+        int numero = 5;
+		
+        //Imprimindo o numero negativo
+        System.out.println(- numero);
+        
+        //incrementando numero em mais 1 numero, imprime 6
+        numero ++;
+        System.out.println(numero);
+        
+        //incrementando numero em mais 1 numero, imprime 7
+        System.out.println(numero ++);// ops algo de errado não está certo
+        //A soma ocorre após a leitura do valor > Le 6 e soma +1
 
-        //O operador de adição (+), quando utilizado em variáveis do 
-        //tipo texto, realizará a “concatenação de textos”.
-        String nomeCompleto = "LINGUAGEM" + " JAVA";
-		System.out.println(nomeCompleto);
-        //qual o resultado das expressoes abaixo?
-        String concatenacao ="?"; 
-        System.out.println(concatenacao);// ?
-
-        concatenacao = 1+1+1+"1";
-        System.out.println(concatenacao);// 31
-
-        concatenacao = 1+"1"+1+1;
-        System.out.println(concatenacao);// 1111
-
-        concatenacao = 1+"1"+1+"1";
-        System.out.println(concatenacao);// 1111
-
-        concatenacao = "1"+1+1+1;
-        System.out.println(concatenacao);// 1111
-
-        concatenacao = "1"+(1+1+1);
-        System.out.println(concatenacao);// 13 = "texto" + (1+1+1)
-
-        //Quando a IDE detecta um texto " ", ele não faz mais operações
-        //Aritmeticas, apenas caso elas ocorram antes do texto ou que sejam
-        //colocados em ( )
+        System.out.println(numero);// agora sim, numero virou 7
+        
+        //ordem de precedencia conta aqui
+        System.out.println(++ numero);
+        //Aqui ele soma 1 + numero e então le o valor que é 8
+        
+        boolean verdadeiro = true;
+        
+        System.out.println("Inverteu " + !verdadeiro);
     }
 }
