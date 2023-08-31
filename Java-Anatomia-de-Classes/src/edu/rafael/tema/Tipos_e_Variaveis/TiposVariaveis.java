2ae6775675c5962c399f779d1b297d9fc6eda3b6
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-08-31 16:19:46 -0300

    Aula3_Tipos_e_Variaveis_Java

--- Java-Anatomia-de-Classes/src/edu/rafael/tema/Tipos_e_Variaveis/TiposVariaveis.java
+++ Java-Anatomia-de-Classes/src/edu/rafael/tema/Tipos_e_Variaveis/TiposVariaveis.java
@@ -2,6 +2,9 @@ package edu.rafael.tema.Tipos_e_Variaveis;
 
 public class TiposVariaveis {
     public static void main(String[] args) {
-        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;//(short) é casting que 
+        //converte uma variavel mais abrangente em um tipo mais específico
     }
 }
