f8abedfab519b4d4e2d970db0f523d7ce142e184
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-09-01 13:56:49 -0300

    Aula6_Operadores_Java

--- Java-BootCamp-Santander/src/edu/rafael/tema/Operadores/Operadores.java
+++ Java-BootCamp-Santander/src/edu/rafael/tema/Operadores/Operadores.java
@@ -2,21 +2,23 @@ package edu.rafael.tema.Operadores;
 
 public class Operadores {
     public static void main(String[] args) {
-        //Ternário
-        int a, b;
-        a = 5;
-        b = 6;
-        String resultado;
-        //EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
-        if (a==b){resultado = "verdadeiro";}
-        else {resultado = "falso";}
-        System.out.println(resultado);
+    int numero1 = 1;
+    int numero2 = 2;
 
-        //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
-        resultado = (a==b) ? "verdadeiro" : "false";
-        System.out.println(resultado);
-        
-        resultado = (a != b) ? "Verdadeiro":"Falso";//!= é diferente
-        System.out.println(resultado);
+    if(numero1 > numero2)
+	System.out.println("Numero 1 maior que numero 2");
+    else System.out.println("Numero 1 não é maior que numero 2");
+
+    if(numero1 < numero2)
+	System.out.println("Numero 1 menor que numero 2");
+
+    if(numero1 >= numero2)
+	System.out.println("Numero 1 maior ou igual que numero 2");
+
+    if(numero1 <= numero2)
+	System.out.println("Numero 1 menor ou igual que numero 2");
+
+    if(numero1 != numero2)
+	System.out.println("Numero 1 é diferente de numero 2");
     }
 }
