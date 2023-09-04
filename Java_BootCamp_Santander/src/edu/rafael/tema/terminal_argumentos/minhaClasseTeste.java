a0132257e4b4ab36297b651fcb39f3e3322dc6de
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-09-04 16:59:17 -0300

    Mudança de nome

--- Java_BootCamp_Santander/src/edu/rafael/tema/terminal_argumentos/minhaClasseTeste.java
+++ Java_BootCamp_Santander/src/edu/rafael/tema/terminal_argumentos/minhaClasseTeste.java
@@ -1,6 +1,6 @@
 package edu.rafael.tema.terminal_argumentos;
 
-public class minhaClasse {
+public class minhaClasseTeste {
     public static void main(String[] args) {
         System.out.println("\nOi, fui executado pelo Terminal\n");
     }
