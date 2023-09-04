c1709546e507ed5102fa7a0e59d27570e3d0ddaa
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-09-04 16:37:27 -0300

    Aula1_Terminal_e_Argumentos

---
+++ Java-BootCamp-Santander/src/edu/rafael/tema/TerminalEArgumentos/MinhaClasse.java
@@ -0,0 +1,7 @@
+package edu.rafael.tema.TerminalEArgumentos;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("\nOi, fui executado pelo Terminal\n");
+    }
+}
