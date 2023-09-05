5746318005098779ec5cd2d0b172295a9af66802
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-09-05 10:14:47 -0300

    Aula3_Terminais_Argumentos

--- Java_BootCamp_Santander/src/edu/rafael/tema/terminal_argumentos/minhaClasseTeste.java
+++ Java_BootCamp_Santander/src/edu/rafael/tema/terminal_argumentos/minhaClasseTeste.java
@@ -1,7 +1,20 @@
 package edu.rafael.tema.terminal_argumentos;
 
 public class minhaClasseTeste {
-    public static void main(String[] args) {
-        System.out.println("\nOi, fui executado pelo Terminal\n");
+    /**
+     * @param args
+     */
+    public static void main (String[] args) {
+        //os argumentos começam com indice 0
+        String nome = "Rafael";
+        String sobrenome = "Martone";
+        nome = args [0];
+        sobrenome = args [1];
+        int idade = Integer.valueOf(args[2]); //vamos falar sobre Wrappers
+        double altura = Double.valueOf(args[3]);
+
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + " cm ");
     }
 }
