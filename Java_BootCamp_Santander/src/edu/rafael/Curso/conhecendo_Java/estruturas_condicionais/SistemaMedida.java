da211f8511ad606a8a5cb188b360fdd67b940c00
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-09-08 20:29:08 -0300

    estruturas_condicionais_encadeada

---
+++ Java_BootCamp_Santander/src/edu/rafael/Curso/conhecendo_Java/estruturas_condicionais/SistemaMedida.java
@@ -0,0 +1,19 @@
+package edu.rafael.Curso.conhecendo_Java.estruturas_condicionais;
+
+import java.util.Scanner;
+
+// Modo condicional if/else
+public class SistemaMedida {
+	public static void main(String[] args) {
+		String sigla = "M";
+        if(sigla == "P")
+			    System.out.println("PEQUENO");
+		    else if(sigla == "M")
+			    System.out.println("MÉDIO");
+		    else if(sigla == "G")
+			    System.out.println("GRANDE");
+		    else
+			    System.out.println("INDEFINIDO"); 	
+		
+	}
+}
