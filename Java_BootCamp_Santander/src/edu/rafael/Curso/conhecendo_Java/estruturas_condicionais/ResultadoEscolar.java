1c45fb3430d74078738d541c5e5f779e155b583e
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-09-08 20:14:38 -0300

    estruturas_condicionais_composta_parte4

--- Java_BootCamp_Santander/src/edu/rafael/Curso/conhecendo_Java/estruturas_condicionais/ResultadoEscolar.java
+++ Java_BootCamp_Santander/src/edu/rafael/Curso/conhecendo_Java/estruturas_condicionais/ResultadoEscolar.java
@@ -2,8 +2,8 @@ package edu.rafael.Curso.conhecendo_Java.estruturas_condicionais;
 
 public class ResultadoEscolar {
 	public static void main(String[] args) {
-		int nota = 7;
-		String resultado = (( nota >=7 ) ? "Aprovado!" : "Reprovado!");
+		int nota = 6;
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
 		System.out.println(resultado);
-	} 
+	}
 }
