fa7bc0c3165142a9acfd4b5e1985c677b1128d63
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-09-08 20:45:41 -0300

    estruturas_condicionais_swithcase_Parte5_withBreak

--- Java_BootCamp_Santander/src/edu/rafael/Curso/conhecendo_Java/estruturas_condicionais/PlanoOperadora.java
+++ Java_BootCamp_Santander/src/edu/rafael/Curso/conhecendo_Java/estruturas_condicionais/PlanoOperadora.java
@@ -8,12 +8,15 @@ public class PlanoOperadora {
 		switch (plano) {
 			case "T": {
 				System.out.println("5Gb Youtube");
+                break;
 			}
 			case "M": {
 				System.out.println("WhatsApp e Instagram grátis");
+                break;
 			}
 			case "B": {
 				System.out.println("100 minutos de ligação");
+                break;
 			}
             /*A falta de break; permite que o switch continue executando os proximos
             *cases, exemplo acima, caso selecione o T, ele executa os cases M e B
