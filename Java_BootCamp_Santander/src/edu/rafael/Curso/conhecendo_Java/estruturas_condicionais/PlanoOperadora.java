723ad94bfe3e1f8baca61fd8e4eec3fc38656189
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-09-08 20:44:49 -0300

    estruturas_condicionais_swithcase_Parte4_noBreak

--- Java_BootCamp_Santander/src/edu/rafael/Curso/conhecendo_Java/estruturas_condicionais/PlanoOperadora.java
+++ Java_BootCamp_Santander/src/edu/rafael/Curso/conhecendo_Java/estruturas_condicionais/PlanoOperadora.java
@@ -1,23 +1,23 @@
 package edu.rafael.Curso.conhecendo_Java.estruturas_condicionais;
 
-// Modo condicional convencional
+// Modo condicional switch/case
 public class PlanoOperadora {
 	public static void main(String[] args) {
-		String plano = "M"; //B ou M ou T
-		
-		if(plano == "B") {
-			System.out.println("100 minutos de ligação");
+		String plano = "T"; // B ou M ou T
 
-		}else if(plano == "M") {
-			System.out.println("100 minutos de ligação");
-			System.out.println("WhatsApp e Instagram grátis");
-            	
-		}else if(plano == "T") {
-			System.out.println("100 minutos de ligação");
-			System.out.println("WhatsApp e Instagram grátis");	
-			System.out.println("5Gb Youtube");	
+		switch (plano) {
+			case "T": {
+				System.out.println("5Gb Youtube");
+			}
+			case "M": {
+				System.out.println("WhatsApp e Instagram grátis");
+			}
+			case "B": {
+				System.out.println("100 minutos de ligação");
+			}
+            /*A falta de break; permite que o switch continue executando os proximos
+            *cases, exemplo acima, caso selecione o T, ele executa os cases M e B
+            */
 		}
-			
-		
 	}
 }
