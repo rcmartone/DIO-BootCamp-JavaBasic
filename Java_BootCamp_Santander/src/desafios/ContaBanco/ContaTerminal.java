668191b625587a1975d4579db0a04959b2ed1d4b
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-09-08 17:38:45 -0300

    Desafio_1_Conta_Banco_V2

--- Java_BootCamp_Santander/src/desafios/ContaBanco/ContaTerminal.java
+++ Java_BootCamp_Santander/src/desafios/ContaBanco/ContaTerminal.java
@@ -1,10 +1,31 @@
 package desafios.ContaBanco;
 import java.util.Scanner;
 
+/**
+* <h1>ContaBanco</h1>
+* A ContaBanco realiza a busca de parametros como número da conta,
+* a agência do cliente, o nome do cliente e por fim, um saldo, 
+*gerando assim uma mensagem final que organiza e exibi os parâmetros em
+*uma estrutura visual ordenada.
+* <p>
+* <b>Note:</b> Leia atentamente a documentação desta classes.
+*
+* @author  Rafael Camacho
+* @version 1.0
+* @since   08/09/2023
+*/
 public class ContaTerminal {
+     /**
+    * @param numberAccount Pede o número da conta do cliente.
+    * @param agency Pede o número da agência do cliente.
+    * @param clienteName Pede o nome do cliente.
+    * @param balance Pede um saldo ao cliente como exemplo.
+    */
     public static void main(String[] args) {
+        //TODO: Conhecer e importar a classe scanner
         Scanner scanner = new Scanner(System.in);
 
+        //TODO: Exibir as mensagens ao usuario em seguida do uso do scanner
         System.out.println("Por favor, digite o número da conta!");
         int numberAccount = scanner.nextInt();
 
@@ -17,6 +38,7 @@ public class ContaTerminal {
         System.out.println("Por favor, digite o saldo em conta!");
         float balance = scanner.nextFloat();
 
+        //TODO: Exibição da mensagem final com a conta criada
         System.out.println("Olá " + clientName + " obrigado por criar uma conta em nosso banco, sua agência é " + agency + " conta " + numberAccount + " e seu saldo de " + balance + " já está disponível para saque.");
     }
 }
