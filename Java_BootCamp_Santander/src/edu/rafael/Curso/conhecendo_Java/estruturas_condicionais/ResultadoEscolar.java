cc0475048ef4afa967c9bfc69992dc472882a4f6
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-09-08 20:10:30 -0300

    estruturas_condicionais_composta_parte1

---
+++ Java_BootCamp_Santander/src/edu/rafael/Curso/conhecendo_Java/estruturas_condicionais/ResultadoEscolar.java
@@ -0,0 +1,14 @@
+package edu.rafael.Curso.conhecendo_Java.estruturas_condicionais;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+       int nota = 10;
+       
+       if(nota >= 7)
+        System.out.println("Aprovado");
+
+       else
+        System.out.println("Reprovado");
+    }
+}
