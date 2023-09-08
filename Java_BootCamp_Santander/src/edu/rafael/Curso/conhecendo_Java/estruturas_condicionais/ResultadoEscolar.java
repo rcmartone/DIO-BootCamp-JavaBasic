3955e91807801d0ea90424d36e37f2fad2c8c90c
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-09-08 20:16:07 -0300

    estruturas_condicionais_Ajuste e colocação de ( )

--- Java_BootCamp_Santander/src/edu/rafael/Curso/conhecendo_Java/estruturas_condicionais/ResultadoEscolar.java
+++ Java_BootCamp_Santander/src/edu/rafael/Curso/conhecendo_Java/estruturas_condicionais/ResultadoEscolar.java
@@ -3,7 +3,7 @@ package edu.rafael.Curso.conhecendo_Java.estruturas_condicionais;
 public class ResultadoEscolar {
 	public static void main(String[] args) {
 		int nota = 6;
-		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		String resultado = ((nota >=7) ? "Aprovado" : ((nota >=5 && nota <7) ? "Recuperação" : "Reprovado"));
 		System.out.println(resultado);
 	}
 }
