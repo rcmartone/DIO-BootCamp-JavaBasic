444e0c432ddaed5b62bada81a47a3ca9a3867b77
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-09-08 21:39:04 -0300

    estruturas_repetição_for_parte5_break

---
+++ Java_BootCamp_Santander/src/edu/rafael/Curso/conhecendo_Java/estruturas_repetição/ExemploBreakContinue.java
@@ -0,0 +1,17 @@
+package edu.rafael.Curso.conhecendo_Java.estruturas_repetição;
+
+// class ExemploBreakContinue.java
+public class ExemploBreakContinue {
+	public static void main(String[] args) {
+	
+	for(int numero = 1; numero <=5; numero ++){
+		if(numero==3)
+			break;
+		
+		System.out.println(numero);
+		
+	}
+	//Qual a saída no console ?
+
+    }
+}
