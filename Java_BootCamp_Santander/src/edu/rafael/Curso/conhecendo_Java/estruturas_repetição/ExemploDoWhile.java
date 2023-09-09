0b1fe848e5d8cca64ab569f752fcbb8a233ecf1c
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-09-08 22:21:46 -0300

    Apenas Teste

--- Java_BootCamp_Santander/src/edu/rafael/Curso/conhecendo_Java/estruturas_repetição/ExemploDoWhile.java
+++ Java_BootCamp_Santander/src/edu/rafael/Curso/conhecendo_Java/estruturas_repetição/ExemploDoWhile.java
@@ -4,22 +4,20 @@ import java.util.Random;
 
 public class ExemploDoWhile {
     /*Estrutura do controle de fluxo do while
-
     do{
         // comando que será executado até que a 
      /  / expressão de validação torne-se falsa 
     }while (expressão booleana de validação);
-
      */
     public static void main(String[] args) {
-        
+
     System.out.println("Discando...");    
     do {
             //excutando repetidas vezes até alguém atender
             System.out.println("Telefone tocando");
-            
+
     }while(tocando());//Enquanto a negação retornada for True, continua executando
-            
+
         System.out.println("Alô !!!");
     }
     /*Metodo booleano que gera randomicamente um valor inteiro que caso seja > 1, é atribuido 
