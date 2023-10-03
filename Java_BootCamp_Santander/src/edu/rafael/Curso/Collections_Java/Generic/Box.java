eac01d255cda0a4494af0d8c69dec7687b4d5269
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-10-03 17:16:48 -0300

    Collections generics part 1

---
+++ Java_BootCamp_Santander/src/edu/rafael/Curso/Collections_Java/Generic/Box.java
@@ -0,0 +1,8 @@
+package edu.rafael.Curso.Collections_Java.Generic;
+
+public class Box {
+    private Object object;
+
+    public void set(Object object) { this.object = object; }
+    public Object get() { return object; }
+}
