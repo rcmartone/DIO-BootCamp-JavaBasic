ab053ef87c4962e777496220740af45ced6430af
Rafael Camacho Martone <rafaelcmartone@gmail.com>
2023-10-21 19:24:54 -0300

    Atualização

--- Java_BootCamp_Santander/src/edu/rafael/Curso/StreamAPI/Functional_Interface/ConsumerExample.java
+++ Java_BootCamp_Santander/src/edu/rafael/Curso/StreamAPI/Functional_Interface/ConsumerExample.java
@@ -14,7 +14,7 @@ public class ConsumerExample {
     // Criar uma lista de números inteiros
     List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
-    // Usar o Consumer com expressão lambda para imprimir números pares
+    // Usar o Consumer com expressão lambida para imprimir números pares
     Consumer<Integer> imprimirNumeroPar = numero -> {
         if (numero % 2 == 0) {
             System.out.println(numero);
